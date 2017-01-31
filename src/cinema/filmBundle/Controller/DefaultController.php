3092f5ba90e829ca312b002243482c5b08a2e153
Cakebool <cakeboool@gmail.com>
2017-01-31 09:37:55 +0100

    creation bundle et mise en place du template de l'accueil

--- src/cinema/filmBundle/Controller/DefaultController.php
+++ src/cinema/filmBundle/Controller/DefaultController.php
@@ -14,4 +14,20 @@ class DefaultController extends Controller
     {
         return $this->render('cinemafilmBundle:Default:index.html.twig');
     }
+    
+    /**
+    * @Route("/films")
+    */
+    public function listAction()
+    {
+        return $this->render('cinemafilmBundle:Default:list.html.twig');
+    }
+    
+    /**
+    * @Route("/film/{id}", requirements={"id": "\d+"})
+    */
+    public function showAction($id)
+    {
+        return $this->render('cinemafilmBundle:Default:show.html.twig');
+    }
 }
